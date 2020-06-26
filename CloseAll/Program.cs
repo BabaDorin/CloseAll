3fe9aa1b6d084ffc2b89166dffc02f1937dc2646
Baba Dorin <bvd.dorin@gmail.com>
2020-06-26 20:25:31 +0300

    Add project files.

---
+++ CloseAll/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CloseAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] runningProcesses = Process.GetProcesses();
+
+            foreach (Process p in runningProcesses)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle)
+                && p.MainWindowTitle != Process.GetCurrentProcess().MainWindowTitle
+                || p.ProcessName == "explorer")
+                    p.Kill();
+            }
+        }
+    }
+}
