eab0e0c3b5f5fe3a0c4ec9cfabc97f1652d28c27
DimitriCunev <61335176+DimitriCunev@users.noreply.github.com>
2020-06-29 01:13:30 +0300

    Add files via upload
    argument support

--- CloseAll/Program.cs
+++ CloseAll/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace CloseAll
 {
@@ -7,8 +8,46 @@ namespace CloseAll
     {
         static void Main(string[] args)
         {
-            Process[] runningProcesses = Process.GetProcesses();
+            bool nofocus = false;
+            bool ignoreStartup = false;
+            bool underException = false;
+            List<string> exceptList = new List<string>();
+            
 
+            foreach (string arg in args)
+            {
+                if (arg[0] == '-')
+                {
+                    underException = false;
+
+                    switch (arg)
+                    {
+                        case "-except":
+                            underException = true;
+                            break;
+                        case "-nofocus":
+                            nofocus = true;
+                            break;
+                        case "-ignore-startup":
+                            ignoreStartup = true;
+                            break;
+                        default:
+                            Console.WriteLine("Unknown argument");
+                            break;
+                    }
+                } else 
+                {
+                    if (underException) {
+                        exceptList.Add(arg);
+                    } 
+
+                }
+            }
+
+            string[] except = exceptList.ToArray(); // Holds process names
+
+
+            Process[] runningProcesses = Process.GetProcesses();
             foreach (Process p in runningProcesses)
             {
                 if (!String.IsNullOrEmpty(p.MainWindowTitle)
