6d417d2a2dedcc79da244481621979a455b0e236
Baba Dorin <bvd.dorin@gmail.com>
2020-06-30 07:45:41 +0300

    Bug fix (-except)

--- CloseAll/Filter.cs
+++ CloseAll/Filter.cs
@@ -46,10 +46,14 @@ namespace CloseAll
 
                         //Nu functionează bine - nu închide applicații cu aceeași denumire ca și windowul focusat.
                         //ar fi bine ca windowurile să se închidă după PID.
+
+                        UnderException = false; 
                         break;
 
                     case "-ignore-startup":
                         IgnoreStartup = true;
+
+                        UnderException = false; 
                         break;
 
                     default:
@@ -68,54 +72,6 @@ namespace CloseAll
             }
 
             return true;
-
-            //
-            // Asta las aici na vseakii slucii
-            //
-            //foreach (string arg in args)
-            //{
-            //    if (arg[0] == '-')
-            //    {
-            //        UnderException = false;
-
-            //        switch (arg.ToLower())
-            //        {
-            //            case "-help":
-            //                Console.WriteLine("\n------------------------ Commands ------------------------\n\n" +
-            //                    "COMMAND: closeall -except <app1> <app2> <app3> : Indicated processes won't get killed\n" +
-            //                    "  Example: closeall -except opera    : All the processes will get killed, except Opera\n" +
-            //                    "  Example: closeall -except discord devenv   : All the processes will get killed, except Discord and Visual Studio\n" +
-            //                    "\nCOMMAND: closeall -ignore-startup : Startup processes won't get killed\n" +
-            //                    "\n----------------------------------------------------------");
-            //                return false;
-
-            //            case "-except":
-            //                UnderException = true;
-            //                break;
-
-            //            case "-nofocus":
-            //                NoFocus = true;
-            //                break;
-
-            //            case "-ignore-startup":
-            //                IgnoreStartup = true;
-            //                break;
-
-            //            default:
-            //                Console.WriteLine(@"Unknown argument   ¯\_('-')_/¯  ");
-            //                Console.WriteLine(@"   Use closeall -help to get the list of commands.");
-            //                return false;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        if (UnderException)
-            //        {
-            //            Except(arg.ToLower());
-            //        }
-            //    }
-            //}
-
         }
 
         public void Except(string processName)
